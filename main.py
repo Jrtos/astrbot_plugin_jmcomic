956a88df22437a6bc74d57ef6daca7da5a98fae9
Jrtos <70319149+Jrtos@users.noreply.github.com>
2025-11-18 00:44:17 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+import astrbot.api.message_components as Comp
 
 import jmcomic
 from jmcomic import *
 
-option = jmcomic.create_option_by_file('D:\work\Python\JMCoic\option.yml')
+option = jmcomic.create_option_by_file('D:\Librury\Desktop\AstrBotLauncher-0.2.0\AstrBot\data\plugins\astrbot_plugin_jmcomic\option.yml')
 client = JmOption.default().new_jm_client()
 
-def download(id: int) -> None:
+def download(id: int):
     jmcomic.download_album(f'{id}', option)
+
+def search_name(name: str, pag: int):
+    page: JmSearchPage = client.search_site(search_query=name, page=pag)
+
+    for album_id, title in page:
+        print(f'[{album_id}]: {title}')
+
+def search_id(id:int):
+    page = client.search_site(search_query=f'{id}')
+    album: JmAlbumDetail = page.single_album
+    print(album.tags)
 
 @register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
 class MyPlugin(Star):
@@ -26,20 +38,37 @@ class MyPlugin(Star):
         message_str = event.message_str
         try:
             album_id = int(message_str.strip('/download '))
-            #download(album_id)
             yield event.plain_result(f"开始下载漫画 {album_id}...")
+            download(album_id)
+            pdf = Comp.File(file=f'download/{album_id}.pdf',name=f'{album_id}.pdf')
+            yield event.chain_result([pdf])
         except ValueError:
             yield event.plain_result("请提供有效的漫画 ID")
 
-    @filter.command("search_id")
+    @filter.command("s_id")
     async def search_id_handler(self, event: AstrMessageEvent):
-        """按 ID 搜索漫画"""
-        pass
+        message_str = event.message_str
+        id = int(message_str.strip('/s_id '))
+        page = client.search_site(search_query=f'{id}')
+        album: JmAlbumDetail = page.single_album
+        resolt = ''
+        for i in album.tags:
+            resolt += f'{i}\n'
+        yield event.plain_result(resolt)
+        
+        
 
-    @filter.command("search_name")
+    @filter.command("s_name")
     async def search_name_handler(self, event: AstrMessageEvent):
-        """按名称搜索漫画"""
-        pass
+        message_str = event.message_str
+        name = message_str.split()
+        page: JmSearchPage = client.search_site(search_query=name[1], page=int(name[2]))
+        yield event.plain_result('正在搜索')
+        resolt = ''
+
+        for album_id, title in page:
+            resolt += f'[{album_id}]: {title}\n'
+        yield event.plain_result(resolt)
 
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
     @filter.command("helloworld")
